cb414a1d0af5acca4a7936d55632c1031df44da8
ibilo-Team <116087712+ibilo-Team@users.noreply.github.com>
2023-03-15 11:09:55 +0330

    add new files and set up some template and static files

--- navak/views.py
+++ navak/views.py
@@ -1,7 +1,7 @@
 from navak import app
-
+from flask import render_template
 
 
 @app.route("/")
 def index_view():
-    return "Hello world"
+    return render_template("index.html")
---
+++ navak_config/config.py
@@ -0,0 +1,43 @@
+import os
+from pathlib import Path
+import datetime
+
+
+# C:\Users\Public\samane-navak  -----> root_path
+BASE_DIR = Path(__file__).parent.parent
+
+
+
+# DB INFO
+USERNAME_DB = "navak"
+PASSWORD_DB = "123654"
+HOST_DB = "localhost"
+PORT_DB = 3307
+NAME_DB = "navak"
+
+
+
+class config:
+    SECRET_KEY = "Hello world!"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{USERNAME_DB}:{PASSWORD_DB}@{HOST_DB}:{PORT_DB}/{NAME_DB}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # session configuration
+    SESSION_TYPE = "filesystem"
+    SESSION_PERMANENT = False
+    PERMANENT_SESSION_LIFETIME = datetime.timedelta(minutes=16)
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = 'Lax'
+    SESSION_COOKIE_NAME = '_session_cookie_'
+
+    # WTF_CSRF_ENABLED = False
+
+
+class Development(config):
+    DEBUG = True
+
+
+class Production(config):
+    DEBUG = False
