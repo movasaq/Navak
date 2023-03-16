8b1f5e33a230a7d0cf978c7c3d0fb876dadbda34
ali sharifi <alisharifyofficial@gmail.com>
2023-03-16 18:55:59 +0330

    kir ro net

--- navak/views.py
+++ navak/views.py
@@ -1,7 +1,27 @@
 from navak import app
 from flask import render_template
-
+import datetime
+from flask import request
 
 @app.route("/")
 def index_view():
     return render_template("index.html")
+
+
+
+
+@app.errorhandler(401)
+def error_401(e):
+    content = {
+        "ip" : request.remote_addr,
+        "time": datetime.datetime.now(),
+    }
+    return render_template("errors/401.html", content=content)
+
+@app.errorhandler(404)
+def error_404(e):
+    content = {
+        "ip" : request.remote_addr,
+        "time": datetime.datetime.now(),
+    }
+    return render_template("errors/404.html", content=content)
--- navak_auth/views.py
+++ navak_auth/views.py
@@ -1,6 +1,6 @@
 from navak_auth import auth
 from navak_auth.utils import employee_login_required
-from flask import (session, abort, render_template)
+from flask import (session, abort, render_template, redirect, url_for, flash)
 from navak_auth.forms import EmployeeLoginForm
 
 @auth.route("/employee/login/")
@@ -12,6 +12,18 @@ def employee_login_view():
     form=EmployeeLoginForm()
     return render_template("auth/login.html", form=form)
 
+@auth.route("/employee/login/", methods=["POST"])
+def employee_login_post():
+    """
+        this view take a post request for login Employees to there panel
+    :return: Html
+    """
+    form = EmployeeLoginForm()
+    if form.validate():
+        pass
+    else:
+        flash("برخی موارد مقدار دهی نشده اند")
+        return redirect(url_for("auth.employee_login_view"))
 
 @auth.route("/login/")
 def login_view():
