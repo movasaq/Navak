420cb50b6864ce02b33c13b37177a29c0b004fa2
ali sharifi <alisharifyofficial@gmail.com>
2023-03-20 18:28:11 +0330

    delete old static js files

--- navak_admin/private_static/js/all_projects.js
+++
@@ -1,7 +0,0 @@
-let rows = document.querySelectorAll(".table-row")
-rows.forEach((each) => {
-    each.style.cursor = "pointer"
-    each.addEventListener("click", (e) => {
-        window.location.href = "/admin/project/details/" + (e.currentTarget.dataset.target)
-    })
-})
